91323a0188dcc01d5702b729204a3b84565459b6
Indicartans <indica464@gmail.com>
2023-12-13 17:39:14 +0700

    make carousel project

--- js/script.js
+++ js/script.js
@@ -1,11 +1,21 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const slides = document.querySelectorAll(".projects-slide");
+  const slides = document.querySelectorAll(".carousel-item");
   let currentSlide = 0;
 
   function showSlide(index) {
     slides.forEach((slide, i) => {
       slide.style.display = i === index ? "block" : "none";
     });
+
+    // slides.forEach((slide, i) => {
+    //   if (i === index) {
+    //     slide.style.opacity = "1";
+    //     slide.style.visibility = "visible";
+    //   } else {
+    //     slide.style.opacity = "0";
+    //     slide.style.visibility = "hidden";
+    //   }
+    // });
   }
 
   function nextSlide() {
@@ -21,6 +31,6 @@ document.addEventListener("DOMContentLoaded", function () {
   showSlide(currentSlide);
 
   // Tambahkan event listener untuk tombol next dan prev
-  document.getElementById("nextBtn").addEventListener("click", nextSlide);
-  document.getElementById("prevBtn").addEventListener("click", prevSlide);
+  document.getElementById("next-btn").addEventListener("click", nextSlide);
+  document.getElementById("prev-btn").addEventListener("click", prevSlide);
 });
